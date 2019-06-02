02a50736b2fffa22be073b2547cad0d69c292644
Josh Juneau <juneau001@gmail.com>
2019-06-02 00:26:01 -0500

    Release 1.0.0

---
+++ target/classes/archetype-resources/src/main/java/resource/JavaEE8Resource.java
@@ -0,0 +1,21 @@
+package ${groupId}.resource;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Juneau
+ */
+@Path("javaee8")
+public class JavaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
